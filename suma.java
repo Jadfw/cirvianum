c0dbb0c0ca26be0b9b76c3a9745005a845467e6c
Joel Arraez <jadfw97@gmail.com>
2019-09-27 16:40:22 +0200

    Castellano suma.java

--- suma.java
+++ suma.java
@@ -2,9 +2,9 @@
 int numero1 = 0;
 int numero2 = 0;
 
-System.out.println("Introdueix el primer nombre");
+System.out.println("Introduce el primer numero");
 numero1 = reader.nextInt();
 
-System.out.println("Introdueix el segon nombre");
+System.out.println("Introduce el segundo numero");
 numero2 = reader.nextInt();
 
